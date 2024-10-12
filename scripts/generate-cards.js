e9bd53c13cd65c031b3cce075c69b54e4f9877af
hardik <hardikmutha2005@gmail.com>
2024-10-12 12:36:08 +0530

    First Commit

---
+++ scripts/generate-cards.js
@@ -0,0 +1,98 @@
+const krafton = [
+  {
+    title: "Title Sponsor",
+    sponsorImageSrc: "../sponsor-images/krafton.png",
+    name: "Krafton",
+    link: "https://krafton.com/en/"
+  },
+  {
+    title: "Pixelverse Sponsor",
+    sponsorImageSrc: "../sponsor-images/krafton.png",
+    name: "Krafton",
+    link: "https://krafton.com/en/"
+  }
+];
+
+const autodesk = [{
+      title: "Associate Sponsor",
+      sponsorImageSrc: "../sponsor-images/autodesk.png",
+      name: "Autodesk",
+      link: "https://www.autodesk.com/"
+  },
+  {
+      title: "Fusion ID Sponsor",
+      sponsorImageSrc: "../sponsor-images/autodesk.png",
+      name: "Autodesk",
+      link: "https://www.autodesk.com/"
+  }
+];
+
+const tietoevry = {
+  title: "Chief Sponsor",
+  sponsorImageSrc: "../sponsor-images/TE.png",
+  name: "Tietoevry",
+  link: "https://www.tietoevry.com/en/"
+};
+
+const wipro = {
+  title: "Chief Sponsor",
+  sponsorImageSrc: "../sponsor-images/wipro.png",
+  name: "Wipro PARI",
+  link: "https://www.wipropari.com/"
+};
+
+const wpm = {
+  title: "Struktura Sponsor",
+  sponsorImageSrc: "../sponsor-images/wpm.png",
+  name: "Walter P Moore",
+  link: "https://www.walterpmoore.com/"
+};
+
+const bhide = [{
+      title: "Associate Sponsor",
+      sponsorImageSrc: "../sponsor-images/bhide.png",
+      name: "Bhide & Bhadane Realties",
+      link: "#"
+  },
+  {
+      title: "Robotica Sponsor",
+      sponsorImageSrc: "../sponsor-images/bhide.png",
+      name: "Bhide & Bhadane Realties",
+      link: "#"
+  }
+];
+
+const AA = {
+  title: "Educational Partner",
+  sponsorImageSrc: "../sponsor-images/AA.png",
+  name: "Acharya Academy",
+  link: "https://acharyaacademy.in/"
+};
+
+function createSponsorCard(sponsor) {
+  document.write(`
+     <div class="card">
+        
+        <div class = "circles">
+          <div class="circle-1"></div>
+          <div class="circle-2"></div>
+          <div class="circle-3"></div>
+        </div> 
+
+        <div class="card-inner"> 
+          <div class="card-front">       
+              <a href = "${sponsor.link}">
+                <img src="${sponsor.sponsorImageSrc}" alt = "${sponsor.name} Sponsor">  
+              </a>
+          </div>
+          <h2 class = "sponsor-title">${sponsor.title}</h2>
+          <div class="card-back">
+            
+            <div class = 'card-info'> 
+              <h3 class = "sponsor-name">${sponsor.name}</h3>
+            </div> 
+          </div>
+        </div> 
+      </div>
+     `);
+}
